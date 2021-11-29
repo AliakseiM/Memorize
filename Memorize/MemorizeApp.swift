57184fcce8127c0da578d5f0a794ba6b08d8a990
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2021-11-29 22:49:24 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Алексей Михайловский on 29.11.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
