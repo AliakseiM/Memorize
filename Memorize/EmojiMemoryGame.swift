607aecc61d6939f05a5003c0a820d9cfa81b72bc
Alexey Mihailovsky <alexey.mihailovsky@gmail.com>
2021-12-01 01:14:15 +0300

    add Model and ViewModel according to MVVM design paradigm

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,25 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Алексей Михайловский on 1.12.21.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    static let emojis = ["✈️", "🚄", "🚀", "🚁", "🛻", "🚛", "🚜" , "🛵", "🚘", "🚠", "⛴", "🚲", "🚡", "🚃", "⛵️", "🚤", "🛸", "🏍", "🛴", "🚢", "🛶", "🚂", "🛰", "🚌"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
---
+++ Memorize/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Memorize
+//
+//  Created by Алексей Михайловский on 1.12.21.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            var content = createCardContent(pairIndex)
+            
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+}
